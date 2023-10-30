434a725b0a55f23dc0f5380bba65657157c2be78
Isabelle <loariyafiaba@gmail.com>
2023-10-30 00:16:46 +0000

    Added the hiding of the options!

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-    //  Variables
-    const checkmarks = document.querySelectorAll(".checkmark");
+	//  Variables
+	const checkmarks = document.querySelectorAll(".checkmark");
+	const optionsH3 = document.querySelectorAll(".options_container h3");
 
+	optionsH3.forEach((h3) => {
+		h3.addEventListener("click", () => {
+			let container = h3.parentElement;
+			let i = h3.querySelector("i");
+			let select_wrapper = container.querySelector(".select_wrapper");
 
+			if (select_wrapper.classList.contains("hide")) {
+				select_wrapper.classList.remove("hide");
 
-
-
-	
+				i.classList.remove("fa-caret-down");
+				i.classList.add("fa-caret-up");
+			} else {
+                select_wrapper.classList.add("hide");
+                i.classList.remove("fa-caret-up");
+				i.classList.add("fa-caret-down");
+			}
+		});
+	});
 
 	checkmarks.forEach((checkmark) => {
 		checkmark.addEventListener("click", function () {
